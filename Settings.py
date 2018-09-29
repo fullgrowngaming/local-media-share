3df3a47b80ea195aab1982054c251e936b689a0b
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2018-09-28 21:42:33 -0700

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,6 +1,6 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
-PASS = 'oauth:6ot0mzk0bkifp2vdmsu44aqlroz6m6'
+PASS = 'oauth:6ot0mzk0bkifp2vdmsu44aqlroz6m6' #no longer active
 NICK = 'kylfbot'
 CHANNEL = 'fullgrowngaming'
 PONG = 'PONG :tmi.twitch.tv' + '\r\n'
